53f89ef547373d5d2af844757f98053dbab071d8
chachacha12 <chaminwoo369@gmail.com>
2024-01-02 14:30:40 +0900

    클라이언트에서 post요청 데이터  보낸것 서버에서 받기

--- server.js
+++ server.js
@@ -4,14 +4,18 @@ const app = express()
 
 //폴더를 server.js에 등록해두어야 폴더안의 css, js파일, 이미지 파일 등의 파일들을 html에서 사용가능 
 app.use(express.static(__dirname+'/public'))
-//ejs세팅끝 - html문서에 DB데이터 꽂기위한 작업
+
+//ejs세팅끝 - (html문서에 DB데이터 꽂기위한 작업)
 //html파일에 데이터 넣고 싶으면 .ejs파일로 만들면 가능. html파일과 거의 같다고 보면됨(views폴더 만들고 그 안에 넣는게 국룰)
 app.set('view engine','ejs')
+
+//클라이언트에서 보낸 데이터 서버에서 받아보려면 이거필요 즉, 요청.body쓰려면 이거 필요 - 그냥 필수적으로 쓰고 시작하면됨
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 
 //몽고디비를 서버와 연결해주는 코드 - url값은 몽고디비 -database- connect-drivers에 있는 링크넣기. 그후 내 db접속용 아이디+비번 링크중간에 넣기
 const { MongoClient } = require('mongodb')
-
 let db
 const url = 'mongodb+srv://chachacha:chaminwoo0106!@cluster0.az9nsfq.mongodb.net/?retryWrites=true&w=majority'
 new MongoClient(url).connect().then((client)=>{
@@ -60,8 +64,16 @@ app.get('/time', async (요청, 응답)=> {
 
 })
 
+//누군가 /write로 접속시 wirte.ejs파일 보여주기
+app.get('/write', (요청, 응답)=> {
+  응답.render('write.ejs') 
+})
 
+//클라이언트에서 /add로 post메소드로 보낸 데이터를 서버가 받아줌
+app.post('/add', (요청, 응답)=> {
+  console.log(요청.body)  //이러면 자바스크립트 object자료형으로 출력됨 
 
+})
 
 
 

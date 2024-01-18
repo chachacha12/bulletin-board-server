5c020d25ec6f8b85ea79de976e626d300d98c6f6
chachacha12 <chaminwoo369@gmail.com>
2024-01-18 20:31:17 +0900

    검색기능 2,3 (search index)

--- server.js
+++ server.js
@@ -313,8 +313,17 @@ app.post('/register', async (요청, 응답)=> {
 
 
 
+
 app.get('/search', async (요청, 응답)=>{
-  let result = await db.collection('post').find({title : {$regex : 요청.query.val} }).toArray()
+
+  let 검색조건 = [
+    {$search : {
+      index : 'title_index',
+      text : { query : 요청.query.val, path : 'title' }
+    }}
+  ]
+
+  let result = await db.collection('post').aggregate(검색조건).toArray()
   응답.render('search.ejs', {글목록 : result})
 }) 
 
